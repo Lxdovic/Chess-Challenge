fc78b21d32c0b9ddc6cd3f377df48d79835b8f58
Lxdovic <ludovicdebever0@gmail.com>
2023-09-28 08:49:49 +0200

    V3: +BetterCheckmateDetection +AspirationWindows +TimeCheckingImprovement

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -3,52 +3,211 @@ using System;
 
 namespace ChessChallenge.Example
 {
-    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-    // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        private Timer _timer;
+        private Board _board;
+        private readonly int[] _pieceWeights = { 0, 100, 310, 330, 500, 1000, 10000 };
+        private readonly int[] _gamePhase = { 0, 0, 1, 1, 2, 4, 0 };
+
+        private readonly ulong[] _pestoTables =
+        {
+            657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569,
+            366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421,
+            366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430,
+            402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514,
+            329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759,
+            291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181,
+            402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804,
+            347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047,
+            347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538,
+            384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492,
+            347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100,
+            366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863,
+            419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932,
+            329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691,
+            383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375,
+            329978099633296596, 67159620133902
+        };
+
+        private Move _iterationMove;
+
+        private enum MoveFlag
+        {
+            Alpha,
+            Exact,
+            Beta
+        }
+
+        private record struct TranspositionTableEntry(ulong Key, int Score, int Depth, MoveFlag Flag, Move Move);
+
+        private readonly TranspositionTableEntry[] _transpositionTable = new TranspositionTableEntry[0x7FFFFF];
 
         public Move Think(Board board, Timer timer)
         {
-            Move[] allMoves = board.GetLegalMoves();
+            _timer = timer;
+            _board = board;
 
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+            Move bestMove = Move.NullMove;
 
-            foreach (Move move in allMoves)
+            int alpha = -100000;
+            int beta = 100000;
+
+            for (int currentDepth = 1; currentDepth <= 100; currentDepth++)
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
+                int iterationScore = Negamax(alpha, beta, currentDepth, 0);
+
+                if (_timer.MillisecondsElapsedThisTurn > _timer.MillisecondsRemaining / 30) break;
+
+                bestMove = _iterationMove;
+
+                if (iterationScore <= alpha || iterationScore >= beta)
                 {
-                    moveToPlay = move;
-                    break;
+                    alpha = -100000;
+                    beta = 100000;
+                    currentDepth--;
+                    continue;
                 }
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+                alpha = iterationScore - 50;
+                beta = iterationScore + 50;
 
-                if (capturedPieceValue > highestValueCapture)
+                // Console.WriteLine("depth {0,1} time {1,5}, pv {2}{3} score {4}",
+                //     currentDepth,
+                //     timer.MillisecondsElapsedThisTurn,
+                //     bestMove.StartSquare.Name,
+                //     bestMove.TargetSquare.Name,
+                //     iterationScore
+                // );
+
+                if (iterationScore > 50000)
+                    break;
+            }
+
+            return bestMove;
+        }
+
+        private int Negamax(int alpha, int beta, int depth, int ply)
+        {
+            bool isRoot = ply == 0;
+
+            if (!isRoot && _board.IsRepeatedPosition()) return 0;
+
+            if (depth == 0) return Quiesce(alpha, beta, 3);
+
+            ulong positionKey = _board.ZobristKey;
+
+            TranspositionTableEntry entry = _transpositionTable[positionKey % 0x7FFFFF];
+
+            if (entry.Key == positionKey && !isRoot && entry.Depth >= depth &&
+                (entry.Flag == MoveFlag.Exact ||
+                 (entry.Flag == MoveFlag.Alpha && entry.Score <= alpha) ||
+                 (entry.Flag == MoveFlag.Beta && entry.Score >= beta))) return entry.Score;
+
+            Move[] moves = _board.GetLegalMoves();
+            Move bestMove = Move.NullMove;
+            int[] moveScores = new int[moves.Length];
+            int bestScore = -100000;
+            int startAlpha = alpha;
+
+            for (int i = 0; i < moves.Length; i++)
+                moveScores[i] = moves[i] == entry.Move ? 100000 :
+                    moves[i].IsCapture ? 100 * (int)moves[i].CapturePieceType - (int)moves[i].MovePieceType :
+                    moves[i].IsPromotion ? (int)moves[i].PromotionPieceType : 0;
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                if (_timer.MillisecondsElapsedThisTurn > _timer.MillisecondsRemaining / 30) return 0;
+
+                for (int j = i + 1; j < moves.Length; j++)
+                    if (moveScores[i] < moveScores[j])
+                        (moves[i], moves[j], moveScores[i], moveScores[j]) =
+                            (moves[j], moves[i], moveScores[j], moveScores[i]);
+
+                Move move = moves[i];
+
+                _board.MakeMove(move);
+                int score = -Negamax(-beta, -alpha, depth - 1, ply + 1);
+                _board.UndoMove(move);
+
+                if (score > bestScore)
                 {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
+                    bestScore = score;
+                    bestMove = move;
+
+                    alpha = Math.Max(alpha, bestScore);
+
+                    if (isRoot) _iterationMove = move;
+                    if (alpha >= beta) break;
                 }
             }
 
-            return moveToPlay;
+            if (moves.Length == 0) return _board.IsInCheckmate() ? -100000 + ply : 0;
+
+            MoveFlag flag = bestScore >= beta ? MoveFlag.Beta :
+                bestScore > startAlpha ? MoveFlag.Exact : MoveFlag.Alpha;
+
+            _transpositionTable[positionKey % 0x7FFFFF] =
+                new TranspositionTableEntry(positionKey, bestScore, depth, flag, bestMove);
+
+            return bestScore;
         }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
+        private int GetPstValue(int psq)
         {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
+            return (int)(((_pestoTables[psq / 10] >> (6 * (psq % 10))) & 63) - 20) * 8;
+        }
+
+        private int EvaluatePeStO(Board board)
+        {
+            if (board.IsInCheckmate())
+                return board.IsWhiteToMove ? 10000 - (board.PlyCount) : -10000 + board.PlyCount;
+
+            int phase = 0, mg = 0, eg = 0;
+
+            foreach (bool isWhite in new[] { true, false })
+            {
+                for (var pieceType = PieceType.Pawn; pieceType <= PieceType.King; pieceType++)
+                {
+                    int piece = (int)pieceType;
+                    ulong pieceBitBoard = board.GetPieceBitboard(pieceType, isWhite);
+
+                    while (pieceBitBoard != 0)
+                    {
+                        phase += _gamePhase[piece];
+                        int index = 128 * (piece - 1) + BitboardHelper.ClearAndGetIndexOfLSB(ref pieceBitBoard) ^
+                                    (isWhite ? 56 : 0);
+                        mg += GetPstValue(index) + _pieceWeights[piece];
+                        eg += GetPstValue(index + 64) + _pieceWeights[piece];
+                    }
+                }
+
+                mg = -mg;
+                eg = -eg;
+            }
+
+            return (mg * phase + eg * (24 - phase)) / 24 * (board.IsWhiteToMove ? 1 : -1);
+        }
+
+        // https://www.chessprogramming.org/Quiescence_Search
+        private int Quiesce(int alpha, int beta, int limit)
+        {
+            int standPat = EvaluatePeStO(_board);
+            if (limit == 0) return standPat;
+            if (standPat >= beta) return beta;
+            if (alpha < standPat) alpha = standPat;
+
+            foreach (Move capture in _board.GetLegalMoves(true))
+            {
+                _board.MakeMove(capture);
+                int score = -Quiesce(-beta, -alpha, limit - 1);
+                _board.UndoMove(capture);
+
+                if (score >= beta) return beta;
+                if (score > alpha) alpha = score;
+            }
+
+            return alpha;
         }
     }
 }
